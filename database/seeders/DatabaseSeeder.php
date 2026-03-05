0858c931715a99599559cb0fd9691048afb90595
Alex4747-J <npanh1903@gmail.com>
2026-03-04 20:13:59 -0500

    Create: Book model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        //BookSeeder::class === Database\Seeders\BookSeeder
+        $this->call([        
+            BookSeeder::class
+        ]);
     }
 }
