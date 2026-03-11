a318b270c37a54ca66d9613d785953e31d069387
Alex4747-J <npanh1903@gmail.com>
2026-03-11 19:22:07 -0400

    Add: base url to API

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {
-        return json_encode([]);
+
+        //return book:all();
+        //return book:all()->toArray();
+        //return json_encode(book:all()->toArray());
+        //return book:all()->toJson();
+
+        $books= Book::all()->toArray();
+        return json_encode([$books]);
+    }
+
+    //Dependency Injection: Laravel will automatically inject the $id parameter from the route into this method
+    public function show(Book $book)
+    {
+        // dd($book);
+    //    $book = Book::find($id);
+       return $book;
+       
     }
 }
