ad9bcbfaa94c5b2141a9e818170c752ff361d842
Alex4747-J <npanh1903@gmail.com>
2026-03-11 20:30:23 -0400

    Add Author id filter on /api/Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,17 +7,37 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
-    {
 
+    /**
+     * return a list of books
+     *
+     * @return array
+     */
+    public function index(Request $request)
+    {
+        $authorID = $request->get('author_id', null);
+        
+
+        $booksQuery = Book::query(); 
+        if ($authorID !== null) {
+            //$booksQuery->whereAuthorId($authorID);
+            $booksQuery->where('author_id', '=', $authorID);
+        }
         //return book:all();
         //return book:all()->toArray();
         //return json_encode(book:all()->toArray());
         //return book:all()->toJson();
 
-        $books= Book::all()->toArray();
+        // $books= Book::all()->toArray();
+        $books = $booksQuery->get();
         return json_encode($books);
     }
+
+    /** This endpoint shoes a specific book
+     * @param Book $book The book I want to show
+     * @return Book
+     * 
+     */
 
     //Dependency Injection: Laravel will automatically inject the $id parameter from the route into this method
     public function show(Book $book)
