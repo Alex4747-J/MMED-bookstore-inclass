ae4f615206d9de497bf2d6805996a7086fe65b32
Alex4747-J <npanh1903@gmail.com>
2026-03-25 18:43:11 -0400

    Add: UserSeeder top create test user for development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call([        
             AuthorSeeder::class,
             BookSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
